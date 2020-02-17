29684c2092185b8021a5521d4578fce328090329
Vania2006 <krutsvanya@gmail.com>
2020-02-17 20:13:50 +0200

    JS

---
+++ JS/#1/js/main.js
@@ -0,0 +1,87 @@
+// let str="Hello";
+// let num=123;
+// let flag=true;
+// let txt="true";
+
+// console.log(typeof(str));
+// console.log(typeof(num));
+// console.log(typeof(flag));
+// console.log(typeof(txt));
+
+
+// let a=123;
+// let b=true;
+// let c="false";
+// let d=[];
+// let e={};
+
+// console q=456;
+// console w=true;
+// console r="false";
+// console t={};
+// console y=[];
+
+
+// let myAge=13;
+// let daniaAge=11;
+// console.log(myAge+daniaAge)
+
+// let q=108,
+//     w=223,
+//     e=2,
+//     r=5;
+// console.log(q+w-e*r)   
+
+
+// let a1=5 + 3,
+//     a2=5 - 3,
+//     a3=5 * 3,
+//     a4=5 / 3;
+// console.log(a1);
+// console.log(a2);
+// console.log(a3);
+// console.log(a4);
+
+
+// let a6=5 % 3,
+//     a7=3 % 5,
+//     a8=5 + '3',
+//     a9='5' - 3,
+//     a10= 75 + 'кг';
+// alert(a6);
+// alert(a7);
+// alert(a8);
+// alert(a9);
+// alert(a10);
+
+// let height=23,
+//     width=10,
+//     s;
+// s=height*width
+// console.log(s);
+
+
+// let heightC=10,
+//     dC=4,
+//     v;
+// dC=dC / 2;
+// v=3.14 * dC**2 * heightC;
+
+// console.log(v);
+
+
+// let n=3,
+//     m=4,
+//     k;
+// k=Math.sqrt(n**2+m**2)
+
+// console.log(k);
+
+
+// let s=2000,
+//     p=10,
+//     perepl;
+
+// perepl=s * p / 100 * 5;
+
+// console.log(perepl);
