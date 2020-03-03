d540ce030fac69641558750dcf214efca4e7880d
Vania2006 <krutsvanya@gmail.com>
2020-03-03 16:30:01 +0200

    js

---
+++ JS/#3/js/main.js
@@ -0,0 +1,105 @@
+// #1
+// let data=1;
+
+// if (data > 2){
+//     console.log('02');
+// } else {
+//     console.log('1');
+// }
+
+
+// #2
+// let date=1;
+
+// if (date%2==0){
+//     console.log('1');
+// }else {
+//     console.log('upppss');
+// }
+
+
+// #3
+// if (5%2==0){
+//     console.log(5%2 + ' грн');
+// } else{
+//     console.log(5%2 + ' євро');
+// }
+
+
+// #4
+// let MaksAge=55;
+
+// if(MaksAge > 0 && MaksAge <16){
+//     console.log('дитина');
+// }   else if(MaksAge > 17 && MaksAge <60){
+//     console.log('доросоий');
+// }   else if(MaksAge > 61 && MaksAge <100){
+//     console.log('пенціонер');
+// }   else{
+//     console.log('не входить до категорій');
+// }
+
+
+// #5
+// let a,
+//     b;
+// a=Math.floor(Math.random()*(1-0+1))+1;
+// console.log(a);
+
+// b=Math.floor(Math.random()*(1-0+1))+1;
+// console.log(b);
+
+// if (a == b){
+//     console.log('фарт');
+// } else{
+//     console.log('не фарт');    
+// }
+
+
+// #6
+// let name='Ivan',
+//     surname='Kruts';
+// console.log('Name '+name.length);
+// console.log('Surname '+surname.length);
+// Name=name.length;
+// Surname=surname.length;
+
+// if(Name > 4 && Surname > 5){
+//     console.log(Name + Surname);
+// } else{
+//     console.log('Упс');
+// }
+
+
+// #7
+// let a;
+
+// a=Math.floor(Math.random()*(1-5+1))+5;
+// console.log(a);
+
+// if (a==1){
+//     console.log('1');
+// }   else if(a==2){
+//     console.log('2');
+// }   else if(a==3){
+//     console.log('3');
+// }   else if(a==4){
+//     console.log('4');
+// }   else if(a==5){
+//     console.log('5');
+// }
+
+
+// 8
+// let Age = prompt("heAge"),
+//     myAge = prompt("myAge");
+
+// myAge = Number(myAge);
+// Age = Number(Age);
+
+// if(Age > 13){
+//     console.log(Age+myAge);
+// }else{
+//     console.log(Age);
+//     console.log(myAge);
+// }
